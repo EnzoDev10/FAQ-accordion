3696a27192ee5034d007e336ea0832c1f292f810
Enzo Cañete <enzodev4@gmail.com>
2024-01-19 19:25:57 -0300

    mobile version V1
     technically complete(lacks animations).

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
+// * onclick function that takes as an argument the number of the question
+// * and based on the number makes visible the corresponding answer and changes his icon
 function showAnswer(Num) {
   let answer;
   switch (Num) {
     case 1:
-      answer = document.getElementById("answer-one");
+      answerNum = document.getElementById("answer-one");
+      iconNum = document.getElementById("icon-one");
       break;
     case 2:
-      answer = document.getElementById("answer-two");
+      answerNum = document.getElementById("answer-two");
+      iconNum = document.getElementById("icon-two");
       break;
     case 3:
-      answer = document.getElementById("answer-three");
+      answerNum = document.getElementById("answer-three");
+      iconNum = document.getElementById("icon-three");
       break;
     case 4:
-      answer = document.getElementById("answer-four");
+      answerNum = document.getElementById("answer-four");
+      iconNum = document.getElementById("icon-four");
       break;
     default:
-      answer = 0;
+      answerNum = 0;
       console.log("something went wrong with the buttons");
   }
 
-  if (answer.style.display === "none") {
-    answer.style.display = "block";
+  if (answerNum.style.display === "none") {
+    answerNum.style.display = "block";
+    iconNum.src = "assets/images/icon-minus.svg";
   } else {
-    answer.style.display = "none";
+    answerNum.style.display = "none";
+    iconNum.src = "assets/images/icon-plus.svg";
   }
-
 }
