b729cbd83947333d93fc9539c2ae5a867fc23276
Enzo Cañete <enzodev4@gmail.com>
2024-01-23 15:13:41 -0300

    function for icons and answer
    Now works for all the questions

--- main.js
+++ main.js
@@ -2,31 +2,26 @@
 /* TODO: Add focus state so users can select questions with the keyboard DONE */
 /* TODO: check Lighhouse Report DONE */
 /* TODO: Create a function that changes the icon when the first question is clicked DONE */
-/* TODO: Add to the function the ability to add an animation to the answers */
+/* TODO: Add to the function the ability to add an animation to the answers DONE */
+
 // TODO: Make the function affect all the questions
 // TODO: Add hover state for the questions so it looks better
 // TODO: Improve Desktop Design
 // TODO: Finish README
 
-const details = document.querySelector("#first-question");
+const questions = document.querySelectorAll(".question");
 
-details.addEventListener("toggle", function () {
-  const img = details.querySelector(".quest-icon");
-  const answer = details.querySelector(".answer");
+questions.forEach((question) => {
+  question.addEventListener("toggle", function handleClick(event) {
+    const img = question.querySelector(".quest-icon");
+    const answer = question.querySelector(".answer");
 
-  if (details.open) {
-    img.src = "assets/images/icon-minus.svg";
-    answer.classList.toggle("active");
-  } else {
-    img.src = "assets/images/icon-plus.svg";
-    answer.classList.remove("active");
-  }
+    if (question.open) {
+      img.src = "assets/images/icon-minus.svg";
+      answer.classList.toggle("active");
+    } else {
+      img.src = "assets/images/icon-plus.svg";
+      answer.classList.remove("active");
+    }
+  });
 });
-
-/* if (answerNum.style.display === "none") {
-    answerNum.style.display = "block";
-    iconNum.src = "assets/images/icon-minus.svg";
-  } else {
-    answerNum.style.display = "none";
-    iconNum.src = "assets/images/icon-plus.svg";
-  } */
