e03aada1ecdc02837093f6a128d723bf05d0ec14
Enzo Cañete <enzodev4@gmail.com>
2024-01-23 12:07:19 -0300

    Updated function
    Now it changes the icon and adds an animation to the first answer

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 /*  TODO: Improve function that displays the answers DONE*/
 /* TODO: Add focus state so users can select questions with the keyboard DONE */
 /* TODO: check Lighhouse Report DONE */
-
-// TODO: Create a function that changes the icon when the first question is clicked
-// TODO: Add to the function the ability to add an animation to the answers
+/* TODO: Create a function that changes the icon when the first question is clicked DONE */
+/* TODO: Add to the function the ability to add an animation to the answers */
 // TODO: Make the function affect all the questions
 // TODO: Add hover state for the questions so it looks better
 // TODO: Improve Desktop Design
@@ -12,12 +11,15 @@
 const details = document.querySelector("#first-question");
 
 details.addEventListener("toggle", function () {
-  const img = document.getElementById("icon-one");
-  
+  const img = details.querySelector(".quest-icon");
+  const answer = details.querySelector(".answer");
+
   if (details.open) {
     img.src = "assets/images/icon-minus.svg";
+    answer.classList.toggle("active");
   } else {
     img.src = "assets/images/icon-plus.svg";
+    answer.classList.remove("active");
   }
 });
 
